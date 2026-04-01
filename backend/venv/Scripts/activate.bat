f6f3f33b067d909f50dd5152e48be54b06f8ece8
Adrián (Torre) <arodriguezc24@fpcoruna.afundacion.org>
2026-04-01 22:52:56 +0200

    Subimos entorno bien hecho

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\adria\OneDrive\Escritorio\TRABAJOS\CLASE\UltraPresetGaming\UltraPresetGaming\venv
+set VIRTUAL_ENV=C:\Users\adria\OneDrive\Escritorio\TRABAJOS\CLASE\UltraPresetGaming\UltraPresetGaming\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
